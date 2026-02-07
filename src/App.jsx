4374f144277cfc907993809e5c760a8d4d4baf21
strictlyManish <rajzmanish94@gmail.com>
2026-02-07 21:29:18 +0530

    initating new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { Route, Routes } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Product from "./pages/Product"
-import Cart from "./pages/Cart"
+import React from 'react'
 
 function App() {
   return (
-    <div className='bg-gray-900 text-white'>
-      <Navbar/>
-      <Routes>
-        <Route path="/products" element={<Product/>} />
-        <Route path="/cart" element={<Cart/>} />
-      </Routes>
-    </div>
+    <div>App</div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
-import { store } from "./app/store.js";
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <Toaster   position="top-center"/>
+        <Toaster   position="bottom-right"/>
         <App />
       </Provider>
     </BrowserRouter>
