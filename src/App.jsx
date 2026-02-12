9565e4aa2027af4df6767e78cc30c5abf4114e1a
strictlyManish <rajzmanish94@gmail.com>
2026-02-12 20:16:26 +0530

    Initiating a project called Calcuator

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-screen bg-gray-800'>
+      <div>
+        <input type="text" placeholder='0' />
+      </div>
+        <div>
+          <button>C</button>
+          <button>AC</button>
+          <button>%</button>
+          <button>DIV</button>
+        </div>
+    </div>
+  )
+}
+
+export default App
